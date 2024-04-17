c2e760d049f891d3bd83f244af92a16eb650d75f
Mark McIntire <mmcintir@nd.edu>
2024-04-16 22:48:28 -0400

    GUI update

--- gui.py
+++ gui.py
@@ -13,17 +13,43 @@ def rsa_encrypt(plain, e, n):
 
 def update_ui(*args):
     # change available options based on selection
-    if selected_option.get() == "RSA":
-        e_label.pack(side=tk.LEFT , padx=(10, 0))
-        e_entry.pack(side=tk.LEFT, padx=(0, 10))
-        n_label.pack(side=tk.LEFT, padx=(10, 0))
-        n_entry.pack(side=tk.LEFT, padx=(0, 10))
-
+    if selected_option.get() == "RSA" and selected_mode.get() == "Encrypt":
+        e_label.grid(row=1,column=0)        #pack(side=tk.LEFT , padx=(10, 0))
+        e_entry.grid(row=1,column=1)                        #pack(side=tk.LEFT, padx=(0, 10))
+        n_label.grid(row=2,column=0)                        #pack(side=tk.LEFT, padx=(10, 0))
+        n_entry.grid(row=2,column=1)
+    elif selected_option.get() == "RSA" and selected_mode.get() == "Decrypt": #                       pack(side=tk.LEFT, padx=(0, 10))
+        d_label.grid(row=1,column=0)        #pack(side=tk.LEFT , padx=(10, 0))
+        d_entry.grid(row=1,column=1)                        #pack(side=tk.LEFT, padx=(0, 10))
+        n_label.grid(row=2,column=0)                        #pack(side=tk.LEFT, padx=(10, 0))
+        n_entry.grid(row=2,column=1)
     else:
-        e_label.pack_forget()
-        e_entry.pack_forget()
-        n_label.pack_forget()
-        n_entry.pack_forget()
+        e_label.grid_forget()
+        e_entry.grid_forget()
+        n_label.grid_forget()
+        n_entry.grid_forget()
+        
+    if selected_option.get() == "Vigenere":
+        vin_label.grid(row=1,column=0)
+        vin_entry.grid(row=1,column=1)
+    else:
+        vin_label.grid_forget()
+        vin_entry.grid_forget()
+        
+    if selected_option.get() == "3DES":
+        des_label.grid(row=1,column=0)
+        des_entry.grid(row=1,column=1)
+    else:
+        des_label.grid_forget()
+        des_entry.grid_forget()
+
+    if selected_option.get() == "AES":
+        aes_label.grid(row=1,column=0)
+        aes_entry.grid(row=1,column=1)
+    else:
+        aes_label.grid_forget()
+        aes_entry.grid_forget()
+        
 
 def encrypt():
     #get input text entry
@@ -37,65 +63,98 @@ def encrypt():
    
     elif selected_option.get() == "RSA":
         #get e and n values
-        try:
-            e_value = int(e_entry.get())
-        except ValueError:
-            output_entry.delete(0, tk.END)
-            output_entry.insert(0, "must enter e value")
-            return
+        
+        if selected_mode.get() == "Encrypt":
+            try:
+                e_value = int(e_entry.get())
+            except ValueError:
+                output_entry.delete(0, tk.END)
+                output_entry.insert(0, "must enter e value")
+                return
+        else:
+            try:
+                d_value = int(d_entry.get())
+            except ValueError:
+                output_entry.delete(0, tk.END)
+                output_entry.insert(0, "must enter d value")
+                return            
         try:
             n_value = int(n_entry.get())
         except ValueError:
             output_entry.delete(0, tk.END)
-            output_entry.insert(0, "must enter e value")
+            output_entry.insert(0, "must enter n value")
             return
         
-        output_text = rsa_encrypt(input_text, e_value, n_value)
+        if selected_mode.get() == "Encrypt":
+            output_text = rsa_encrypt(input_text, e_value, n_value)
+        else:
+            output_text = rsa_decrypt(input_text, d_value, n_value)
         output_entry.delete(0, tk.END)
         output_entry.insert(0, output_text)
         
 
 root = tk.Tk()
 root.title("Encryption/Decryption")
+root.geometry("1000x600")
+root.config(bg="skyblue", padx=10,pady=10)
 
-#main frame
-frame = tk.Frame(root)
-frame.pack(padx=10, pady=10)
+
 
 #label for text
-input_label = tk.Label(frame, text="Enter Plain Text:")
-input_label.pack(side=tk.LEFT, padx=(0, 10))
+input_label = tk.Label(root, text="Input:", font=("Helvetica", 16))
+input_label.grid(row=0,column=0,padx=5,pady=5)
 
 #text entry
-input_entry = tk.Entry(frame, width=20)
-input_entry.pack(side=tk.LEFT)
+input_entry = tk.Entry(root, width=20)
+input_entry.grid(row=0,column=1 ,padx=5,pady=5)
 
 #dropdown to choose encryption alg
-selected_option = tk.StringVar(frame)
-selected_option.set("Vigenere")  # set default value
-selected_option.trace("w", update_ui)  # Trace changes to this variable
-options_menu = tk.OptionMenu(frame, selected_option, "Vigenere", "RSA")
-options_menu.pack(side=tk.LEFT, padx=(10, 0))
+selected_option = tk.StringVar(root)
+selected_option.set("Select Algorithm") 
+selected_option.trace("w", update_ui) 
+options_menu = tk.OptionMenu(root, selected_option, "Vigenere", "RSA", "3DES", "AES")
+options_menu.grid(row=0, column=2 ,padx=5,pady=5)
 
-#trigger encryption
-#process_button = tk.Button(frame, text="encrypt", command=encrypt)
-#process_button.pack(side=tk.LEFT, padx=(10, 0))
 
-# e, n value (initially hidden)
-e_label = tk.Label(frame, text="e value:")
-e_entry = tk.Entry(frame, width=3)
-n_label = tk.Label(frame, text="n value:")
-n_entry = tk.Entry(frame, width=3)
+
+# e, n, d value (initially hidden)
+e_label = tk.Label(root, text="e value:",font =("Helvetica", 16))
+e_entry = tk.Entry(root, width=3)
+
+d_label = tk.Label(root, text="d value:",font =("Helvetica", 16))
+d_entry = tk.Entry(root, width=3)
+
+n_label = tk.Label(root, text="n value:",font =("Helvetica", 16))
+n_entry = tk.Entry(root, width=3)
+
+
+
+vin_label = tk.Label(root, text ="Vinegere Key",font =("Helvetica", 16))
+vin_entry = tk.Entry(root, width = 10)
+
+des_label = tk.Label(root, text ="DES Key",font =("Helvetica", 16))
+des_entry = tk.Entry(root, width = 10)
+
+aes_label = tk.Label(root, text ="AES Key",font =("Helvetica", 16))
+aes_entry = tk.Entry(root, width = 10)
 
 # Text entry widget for output
 #output_entry = tk.Entry(frame, width=20)
 #output_entry.pack(side=tk.LEFT, padx=(10, 0))
+selected_mode = tk.StringVar(root)
+selected_mode.set("Select Mode",) 
+selected_mode.trace("w", update_ui) 
+mode_options_menu = tk.OptionMenu(root, selected_mode, "Encrypt", "Decrypt")
+mode_options_menu.grid(row=0, column=3 ,padx=5,pady=5)
 
-process_button = tk.Button(frame, text="encrypt", command=encrypt)
-process_button.pack(side=tk.LEFT, padx=(10, 0))
+process_button = tk.Button(root, text="Run Program", command=encrypt,font =("Helvetica", 16))
+process_button.grid(row=0,column=4 ,padx=5,pady=5)
 
-output_entry = tk.Entry(frame, width=20)
-output_entry.pack(side=tk.LEFT, padx=(10, 0))
 
-# Start the application
+output_label = tk.Label(root, text = "Output",font =("Helvetica", 16))
+output_label.grid(row=0, column=5 ,padx=5,pady=5)
+output_entry = tk.Entry(root, width=20)
+output_entry.grid(row=0,column=6 ,padx=5,pady=5)
+
+# Start the applications
 root.mainloop()
